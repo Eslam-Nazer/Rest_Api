293e3ee766253b3295fbd969567ec78624cd8a3c
Eslam-Nazer <eslam.nazer43@gmail.com>
2024-10-15 09:27:16 +0300

    Create Read Single Post Function

--- models/Post.php
+++ models/Post.php
@@ -36,4 +36,30 @@ class Post
 
         return $stmt;
     }
+
+    // Get Single Post
+    public function read_single()
+    {
+        $query  = "SELECT P.*, C.name AS category_name FROM " . $this->table . " P";
+        $query .= " INNER JOIN category C ON P.category_id = C.id ";
+        $query .= " WHERE P.id= :id LIMIT 1";
+
+        // Prepare Statment
+        $stmt = $this->conn->prepare($query);
+
+        // Bind Id
+        $stmt->bindValue(":id", $this->id, PDO::PARAM_INT);
+
+        // Execute Query
+        $stmt->execute();
+
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        // Set Properties
+        $this->title = $row['title'];
+        $this->category_id = $row['category_id'];
+        $this->body = $row['body'];
+        $this->author = $row['author'];
+        $this->category_name = $row['category_name'];
+    }
 }
