a0b4bac906c9a2876865b86e42583e3ced96d847
Eslam-Nazer <eslam.nazer43@gmail.com>
2024-10-15 18:51:46 +0300

    Update Post

--- models/Post.php
+++ models/Post.php
@@ -92,4 +92,37 @@ class Post
         printf('Error: %s.\n', $stmt->error);
         return false;
     }
+
+    // Update Post
+    public function update()
+    {
+        // Update query
+        $query = "UPDATE $this->table SET title=:title , body=:body, author=:author, category_id=:category_id WHERE id=:id";
+
+        // Prepare Statment
+        $stmt = $this->conn->prepare($query);
+
+        // Filtration data
+        $this->id           = htmlspecialchars(strip_tags($this->id));
+        $this->title        = htmlspecialchars(strip_tags($this->title));
+        $this->body         = htmlspecialchars(strip_tags($this->body));
+        $this->author       = htmlspecialchars(strip_tags($this->author));
+        $this->category_id  = htmlspecialchars(strip_tags($this->category_id));
+
+        $params = [
+            ":id"           => $this->id,
+            ":title"        => $this->title,
+            ":body"         => $this->body,
+            ":author"       => $this->author,
+            ":category_id"  => $this->category_id,
+        ];
+
+        // Bind data and execute query
+        if ($stmt->execute($params)) {
+            return true;
+        }
+        // Print error if something goes wrong
+        printf("Error: %s\n", $stmt->error);
+        return false;
+    }
 }
