f1e472976f8ec3023a3c90acea28d60101612345
Eslam-Nazer <eslam.nazer43@gmail.com>
2024-10-15 11:08:13 +0300

    Create Post

--- models/Post.php
+++ models/Post.php
@@ -62,4 +62,34 @@ class Post
         $this->author = $row['author'];
         $this->category_name = $row['category_name'];
     }
+
+    public function create()
+    {
+        $query = 'INSERT INTO ' . $this->table . ' SET title=:title body=:body author=:author category_id=:category_id';
+
+        // Prepare statment
+        $stmt = $this->conn->prepare($query);
+
+        // Filtration data
+        $this->title = htmlspecialchars(strip_tags($this->title));
+        $this->body = htmlspecialchars(strip_tags($this->body));
+        $this->author = htmlspecialchars(strip_tags($this->author));
+        $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+
+        // Bind data
+        $params = [
+            ":title"    => $this->title,
+            ":body"    => $this->body,
+            ":author"    => $this->author,
+            ":category_id"    => $this->category_id,
+        ];
+
+        if ($stmt($params)) {
+            return true;
+        }
+
+        // Print error if something goes wrong
+        printf('Error: %s.\n', $stmt->error);
+        return false;
+    }
 }
