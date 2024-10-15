0e8a2e796d344a0af502800066320cd1ebb38d36
Eslam-Nazer <eslam.nazer43@gmail.com>
2024-10-15 21:50:21 +0300

    Read (Show) Categories

--- models/Category.php
+++ models/Category.php
@@ -16,4 +16,16 @@ class Category
     {
         $this->conn = $db;
     }
+
+    // Get (Read) Categories
+    public function read()
+    {
+        // Make Query
+        $query = "SELECT * FROM $this->table ORDER BY created_at DESC";
+        // Prepare Statment
+        $stmt = $this->conn->prepare($query);
+        // Execute query
+        $stmt->execute();
+        return $stmt;
+    }
 }
