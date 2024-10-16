4db8a157e7bcc303560f5437a4579ced17fdb6cc
Eslam-Nazer <eslam.nazer43@gmail.com>
2024-10-16 20:05:35 +0300

    Read One Category

--- models/Category.php
+++ models/Category.php
@@ -28,4 +28,23 @@ class Category
         $stmt->execute();
         return $stmt;
     }
+
+    public function read_one()
+    {
+        // Make Query
+        $query = "SELECT * FROM $this->table WHERE id=:id LIMIT 1";
+        // prepare Statmt
+        $stmt = $this->conn->prepare($query);
+        // Bind id value
+        $stmt->bindValue(":id", $this->id, PDO::PARAM_INT);
+        // Execute Statment
+        $stmt->execute();
+
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        // Set Properties
+        $this->id           = $row["id"];
+        $this->name         = $row["name"];
+        $this->created_at   = $row["created_at"];
+    }
 }
