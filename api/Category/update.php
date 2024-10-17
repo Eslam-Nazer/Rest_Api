bbdcfaac1c68c9f06cb1fad9d73ff624d406b2c7
Eslam-Nazer <eslam.nazer43@gmail.com>
2024-10-17 22:08:54 +0300

    Update Category With API

---
+++ api/Category/update.php
@@ -0,0 +1,31 @@
+<?php
+// Headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+header("Access-Control-Allow-Methods: PUT");
+header("Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Access-Control-Allow-Origin");
+
+include_once "../../config/Database.php";
+include_once "../../models/Category.php";
+
+// Instantiate DB & Connect
+$database = new Database();
+$db = $database->connect();
+// Instantiate Category Object
+$category = new Category($db);
+// Prepare Data
+$data = json_decode(file_get_contents("php://input"));
+// Prepare Properties
+$category->id   = $data->id;
+$category->name = $data->name;
+
+// Update Category
+if ($category->update()) {
+    echo json_encode([
+        "message"   => "Category Updated"
+    ]);
+} else {
+    echo json_encode([
+        "message"   => "Category Not Updated"
+    ]);
+}
