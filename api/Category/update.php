78ea791b51d06caec403cd15c1887adb41bc8db4
Eslam-Nazer <eslam.nazer43@gmail.com>
2024-10-17 22:27:08 +0300

    Add X-Requested-With For Update Category Headers

--- api/Category/update.php
+++ api/Category/update.php
@@ -3,7 +3,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 header("Access-Control-Allow-Methods: PUT");
-header("Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Access-Control-Allow-Origin");
+header("Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Access-Control-Allow-Origin,X-Requested-With");
 
 include_once "../../config/Database.php";
 include_once "../../models/Category.php";
